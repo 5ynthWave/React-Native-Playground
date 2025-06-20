c0ab7a2c0863da6f51a83e435099fb1c1032a2e0
Erik E. Zuñiga <ezuni3468@outlook.com>
2025-06-20 17:25:22 -0500

    blub

--- react-native-playground/app/index.tsx
+++ react-native-playground/app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, margin: 8 }}>
+      <View style={{ alignItems: "center", flexDirection: "row" }}>
+        <View style={styles.circleOuter}>
+          <View style={styles.circleInner}>
+          </View>
+        </View>
+        <Text style={{ fontSize: 16 }}> Systems Online </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  circleOuter: {
+    width: 32,
+    height: 32,
+    backgroundColor: "lightgreen",
+    borderRadius: 16,
+    opacity: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circleInner: {
+    width: 20,
+    height: 20,
+    backgroundColor: "green",
+    borderRadius: 12,
+    opacity: 0.5,
+  },
+});
